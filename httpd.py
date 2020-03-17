7f013dbababd1d77d330c2bbd5a014b9321ea419
webappfirewall <60896858+webappfirewall@users.noreply.github.com>
2020-03-16 21:29:47 -0600

    Delete httpd.py

--- httpd.py
+++
@@ -1,9 +0,0 @@
-import SimpleHTTPServer
-import SocketServer
-
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-
-httpd = SocketServer.TCPServer(('', 8080), Handler)
-
-print("$$ HTTP Server for test in Web App Firewall v1.0 $$")
-httpd.serve_forever()
