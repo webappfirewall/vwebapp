8c176c7f13ff4aab8f7a249d0ee94b562bd9401e
Jason Rossello <jrossellor1400@alumno.ipn.mx>
2020-03-24 17:56:00 -0600

    vwebapp v1.0.1

--- s_get.php
+++ s_get.php
@@ -1,3 +1,13 @@
 <?php
-  echo "Hello from s_get.php";
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+
+  $conn = new mysqli($servername, $username, $password);
+
+  if($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  echo "Connected successfully";
 ?>
