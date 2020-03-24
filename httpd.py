0c476c572fbbf6f24ff60bf509a4ee58df02ba3f
Jason Rossello <jrossellor1400@alumno.ipn.mx>
2020-03-24 14:05:20 -0600

    vwebapp v1.0.0

--- httpd.py
+++
@@ -1,9 +0,0 @@
-import SimpleHTTPServer
-import SocketServer
-
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-
-httpd = SocketServer.TCPServer(('192.168.17.150', 80), Handler)
-
-print("$$ HTTP Server for test in Web App Firewall v1.0 $$")
-httpd.serve_forever()
