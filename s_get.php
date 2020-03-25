a30fe78d3e10c22beff4b8882554ac55cdae864b
Jason Rossello <jrossellor1400@alumno.ipn.mx>
2020-03-25 09:49:01 -0600

    vwebapp v1.1.3

--- s_get.php
+++ s_get.php
@@ -1,9 +1,9 @@
 <?php
-  $servername = "localhost";
-  $username = "mysql";
-  $password = "";
+  $server = "192.168.17.150";
+  $usr = "mysql";
+  $pwd = "Qw3rt&.12345";
 
-  $conn = mysqli_connect($servername, $username, $password);
+  $conn = mysqli_connect($server, $usr, $pwd);
 
   if(!$conn) {
     die("Connection failed: " . mysqli_connect_error());
