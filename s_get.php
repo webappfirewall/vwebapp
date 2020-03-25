36255551f2c56eef2c7a3d6442f3c72e9f7cf790
Jason Rossello <jrossellor1400@alumno.ipn.mx>
2020-03-24 18:14:59 -0600

    vwebapp v1.0.4

--- s_get.php
+++ s_get.php
@@ -1,6 +1,6 @@
 <?php
-  $servername = "localhost";
-  $username = "root";
+  $servername = "192.168.17.150";
+  $username = "mysql";
   $password = "Qw3rt&.12345";
 
   $conn = mysqli_connect($servername, $username, $pssword);
