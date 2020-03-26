43e2c3ebe5ff76a9bf14962540f3f7d20d56dfff
Jason Rossello <jrossellor1400@alumno.ipn.mx>
2020-03-26 12:10:17 -0600

    vwebapp v1.2.8

--- s_get.php
+++ s_get.php
@@ -3,14 +3,12 @@
   $username = "u_db";
   $password = "Qw3rt&.12345";
 
-  try {
-      $conn = new PDO("mysql:host=$servername;dbname=test", $username, $password);
-      // set the PDO error mode to exception
-      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      echo "Connected successfully";
-      }
-  catch(PDOException $e)
-      {
-      echo "Connection failed: " . $e->getMessage();
-      }
+  // Create connection
+  $conn = mysqli_connect($servername, $username, $password);
+
+  // Check connection
+  if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+  }
+  echo "Connected successfully";
 ?>
