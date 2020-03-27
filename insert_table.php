e57a1c48fd6054bd06c5f67fba61409c0b4b5bd9
Jason Rossello <jrossellor1400@alumno.ipn.mx>
2020-03-27 13:38:40 -0600

    vwebapp v1.4.1

--- insert_table.php
+++ insert_table.php
@@ -11,13 +11,13 @@
       die("Connection failed: " . mysqli_connect_error());
   }
 
-  $sql = "INSERT INTO Products VALUES ( , 'Samsung', 'A 51', 'Smartphone', 1);";
-  $sql .= "INSERT INTO Products VALUES ( , 'Samsung', 'S 20 Ultra', 'Smartphone', 0);";
-  $sql = "INSERT INTO Products VALUES ( , 'Samsung', 'A 71', 'Smartphone', 1);";
-  $sql .= "INSERT INTO Products VALUES ( , 'Samsung', 'S 20+', 'Smartphone', 0);";
-  $sql = "INSERT INTO Products VALUES ( , 'Samsung', 'A 20', 'Smartphone', 0);";
-  $sql .= "INSERT INTO Products VALUES ( , 'Samsung', 'M 10s', 'Smartphone', 1);";
-  $sql .= "INSERT INTO MyGuests VALUES ( , 'Samsung', 'Note 10+', 'Smartphone', 1)";
+  $sql = "INSERT INTO Products VALUES (1 , 'Samsung', 'A 51', 'Smartphone', 1);";
+  $sql .= "INSERT INTO Products VALUES (2 , 'Samsung', 'S 20 Ultra', 'Smartphone', 0);";
+  $sql = "INSERT INTO Products VALUES (3 , 'Samsung', 'A 71', 'Smartphone', 1);";
+  $sql .= "INSERT INTO Products VALUES (4 , 'Samsung', 'S 20+', 'Smartphone', 0);";
+  $sql = "INSERT INTO Products VALUES (5 , 'Samsung', 'A 20', 'Smartphone', 0);";
+  $sql .= "INSERT INTO Products VALUES (6 , 'Samsung', 'M 10s', 'Smartphone', 1);";
+  $sql .= "INSERT INTO MyGuests VALUES (7 , 'Samsung', 'Note 10+', 'Smartphone', 1)";
 
   if (mysqli_multi_query($conn, $sql)) {
       echo "New records created successfully";
