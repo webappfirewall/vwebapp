0fd55ebacf3f902a568ae4aa292f61b0023500a3
Jason Rossello <jrossellor1400@alumno.ipn.mx>
2020-03-27 15:08:12 -0600

    vwebapp v1.4.7

--- s_get.php
+++ s_get.php
@@ -18,11 +18,14 @@
 
   $result = mysqli_query($conn, $sql);
 
-  if ($result) {
-    echo $result;
-  } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-  }
+  if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["id"]. " - Model: " . $row["model"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
 
   mysqli_close($conn);
 ?>
