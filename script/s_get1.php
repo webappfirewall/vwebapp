381b8fe4e0e7b59f715c532c0c1ff561d1ee291b
Jason Rossello <jrossellor1400@alumno.ipn.mx>
2020-04-14 15:38:00 -0500

    vwebapp v1.6.2

--- script/s_get1.php
+++ script/s_get1.php
@@ -11,5 +11,18 @@
   if (!$conn) {
      die("Connection failed: " . mysqli_connect_error());
   }
-  echo "Connected successfully";
+
+  $sql = "SELECT * FROM city";
+  $result = mysqli_query($conn, $sql);
+
+  if (mysqli_num_rows($result) > 0) {
+      // output data of each row
+      while($row = mysqli_fetch_assoc($result)) {
+          echo "Name: " . $row["Name"]. " Population: " . $row["Population"]. "<br>";
+      }
+  } else {
+      echo "0 results";
+  }
+
+  mysqli_close($conn);
 ?>
