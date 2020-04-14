079e8cf114605247a623de1f10bded57671cc4f3
Jason Rossello <jrossellor1400@alumno.ipn.mx>
2020-04-14 18:26:23 -0500

    vwebapp v1.7.0

--- script/s_get1.php
+++ script/s_get1.php
@@ -20,6 +20,9 @@
   if (mysqli_num_rows($result) > 0) {
       // output data of each row
       echo "
+      <head>
+        <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css' integrity='sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh' crossorigin='anonymous'>
+      </head>
       <table class='table'>
         <thead>
           <tr>
