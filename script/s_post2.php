6dc3bd268bed439da1a73a854ea2e2569a7b1f79
Jason Rossello <jrossellor1400@alumno.ipn.mx>
2020-04-21 11:54:52 -0500

    vwebapp v1.9.7

--- script/s_post2.php
+++ script/s_post2.php
@@ -18,7 +18,7 @@
 
     $sql = "INSERT INTO comments (usertag, comment) VALUES ('$userTag', '$comment')";
 
-    if (mysqli_query($sql)) {
+    if (mysqli_query($conn, $sql)) {
       echo "New record created successfully";
     } else {
       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
